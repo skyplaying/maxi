add669c82d3d9e2b99faf2deb71f5e3a368b0c98
yechunsen <1604980041@qq.com>
2023-03-04 14:14:56 +0000

    change genLoading icon

--- src/pages/aigc/AIGCMintContainer.jsx
+++ src/pages/aigc/AIGCMintContainer.jsx
@@ -1,21 +1,21 @@
 import styles from './index.module.scss'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import userInfoIcon from '../../assets/img/page/aigc/InfoUser.png'
 import weijiazai from '../../assets/img/page/home/weijiazai.png'
 import image8 from '../../assets/img/page/aigc/image8.png'
 import image9 from '../../assets/img/page/aigc/image9.png'
 import image10 from '../../assets/img/page/aigc/image10.png'
 import image11 from '../../assets/img/page/aigc/image11.png'
-import {Box, TextField, Button, Typography, ImageList, ImageListItem, Checkbox} from '@mui/material'
+import { Box, TextField, Button, Typography, ImageList, ImageListItem, Checkbox } from '@mui/material'
 import twitterIcon from '../../assets/img/page/product_detail/twitter.png'
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import UploadImageComp from 'src/components/AIGC/UploadImageComp'
 import vector from '../../assets/img/page/aigc/Vector.png'
-import {genaigc, genaigcByPic, getPoint} from "../../service/aigcMint";
-import {ConnectWallet} from "../../web3/useMetaConnect";
-import {useWallet} from '@suiet/wallet-kit';
-import {upload, DataURIToBlob, uploadToNFTStorage} from "../../web3/ipfs";
+import { genaigc, genaigcByPic, getPoint } from "../../service/aigcMint";
+import { ConnectWallet } from "../../web3/useMetaConnect";
+import { useWallet } from '@suiet/wallet-kit';
+import { upload, DataURIToBlob, uploadToNFTStorage } from "../../web3/ipfs";
 import CircularProgress from '@mui/material/CircularProgress';
 import AIGCSuccessModal from 'src/components/AIGC/AIGCSuccessModal'
 import AIGCModal from "../../components/AIGC/AIGCModal";
@@ -32,19 +32,19 @@ const BannerComp = () => {
   return (
     <div className={styles.left}>
       <div className={styles.swiper}>
-        <img className={styles.image8} src={image8} alt=''/>
-        <img className={styles.image9} src={image9} alt=''/>
-        <img className={styles.image10} src={image10} alt=''/>
-        <img className={styles.image11} src={image11} alt=''/>
+        <img className={styles.image8} src={image8} alt='' />
+        <img className={styles.image9} src={image9} alt='' />
+        <img className={styles.image10} src={image10} alt='' />
+        <img className={styles.image11} src={image11} alt='' />
       </div>
     </div>
   )
 }
 
-const ImageCardBox = ({formik, aiImgList}) => {
+const ImageCardBox = ({ formik, aiImgList }) => {
   console.log("aiImgList", aiImgList)
   return (
-    <ImageList sx={{width: 600, height: 600}} cols={2} rowHeight={290}>
+    <ImageList sx={{ width: 600, height: 600 }} cols={2} rowHeight={290}>
       {aiImgList.map((item) => (
         <ImageListItem
           key={item.seed}
@@ -58,8 +58,8 @@ const ImageCardBox = ({formik, aiImgList}) => {
         >
           <div className={styles.aiBox}>
             <img className={item.seed <= 4 ? styles.weijiazai : null}
-                 src={item.pic}
-                 loading="lazy"
+              src={item.pic}
+              loading="lazy"
             />
           </div>
           <Box
@@ -72,10 +72,10 @@ const ImageCardBox = ({formik, aiImgList}) => {
             <Checkbox
               checked={formik.values?.checked === item}
               sx={{
-                '& .MuiSvgIcon-root': {fontSize: 40}
+                '& .MuiSvgIcon-root': { fontSize: 40 }
               }}
-              icon={<RadioButtonUncheckedIcon/>}
-              checkedIcon={<RadioButtonCheckedIcon/>}
+              icon={<RadioButtonUncheckedIcon />}
+              checkedIcon={<RadioButtonCheckedIcon />}
             />
           </Box>
         </ImageListItem>
@@ -84,13 +84,13 @@ const ImageCardBox = ({formik, aiImgList}) => {
   )
 }
 
-const ImageSelectComp = ({formik, aiImgList}) => {
+const ImageSelectComp = ({ formik, aiImgList }) => {
   return (
     <>
-      <Typography variant="body2" sx={{my: 2, fontSize: '16px'}}>
+      <Typography variant="body2" sx={{ my: 2, fontSize: '16px' }}>
         Results:
       </Typography>
-      <ImageCardBox formik={formik} aiImgList={aiImgList}/>
+      <ImageCardBox formik={formik} aiImgList={aiImgList} />
       <div className={styles.tapYourFavorite}>
         Tap your favorite image and mint.
       </div>
@@ -99,16 +99,16 @@ const ImageSelectComp = ({formik, aiImgList}) => {
 }
 
 
-const TextInputComp = ({formik}) => {
+const TextInputComp = ({ formik }) => {
   return (
     <>
       <div className={styles.step1}>
         1. Describe your art
       </div>
       <div className={styles.step1Desc}>
-        Describe your desired image with English. <br/>
-        Examples: <br/>
-        a person as apex legends character, digital illustration portrait design, by android jones <br/> and greg rutkowski, retrowave color scheme, detailed, cinematic lighting, wide angle <br/> action dynamic portrait
+        Describe your desired image with English. <br />
+        Examples: <br />
+        a person as apex legends character, digital illustration portrait design, by android jones <br /> and greg rutkowski, retrowave color scheme, detailed, cinematic lighting, wide angle <br /> action dynamic portrait
       </div>
       <TextField
         className='text-area'
@@ -146,38 +146,38 @@ const InfoComp = () => {
     <>
       <div className={styles.title}>Suicasso AIGC Pilot Collection</div>
       <div className={styles.box1}>
-        <img className={styles.userInfoIcon} src={userInfoIcon} alt=''/>
+        <img className={styles.userInfoIcon} src={userInfoIcon} alt='' />
         <div className={styles.platformBox}>
           {platformList.map((item, index) => {
             return (
               <div className={styles.platformItem} key={index}>
                 <img className={styles.platformIcon} src={item.icon} alt='' onClick={() => {
                   window.open("https://twitter.com/Maxi_sui");
-                }}/>
+                }} />
               </div>
             )
           })}
         </div>
       </div>
       <div className={styles.desc}>
-        Suicasso is the first AIGC NFT on SUI.<br/> <br/>
+        Suicasso is the first AIGC NFT on SUI.<br /> <br />
 
-        Currently, It is live on the SUI devnet but all participants will receive the same NFT when the SUI mainnet launches.<br/> <br/>
+        Currently, It is live on the SUI devnet but all participants will receive the same NFT when the SUI mainnet launches.<br /> <br />
         <div className={styles.descWeight}>
-          The first 1000 of Suicasso will be sent to Maxi supporters for free. <br/>
-          Simply:<br/>
-          1. Follow @maxi_sui on twitter<br/>
-          2. Retweet this tweet<br/>
-          3. Comment your wallet address on the tweet<br/>
-          4. Wait for our moderators to add you to the whitelist(Should take a few minutes)<br/><br/>
+          The first 1000 of Suicasso will be sent to Maxi supporters for free. <br />
+          Simply:<br />
+          1. Follow @maxi_sui on twitter<br />
+          2. Retweet this tweet<br />
+          3. Comment your wallet address on the tweet<br />
+          4. Wait for our moderators to add you to the whitelist(Should take a few minutes)<br /><br />
         </div>
-        Then you can generate the art and mint it below.<br/>
+        Then you can generate the art and mint it below.<br />
       </div>
     </>
   )
 }
 
-const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
+const AIGCMintContainer = ({ formik, userPoint, setOpen, setModalText }) => {
 
   const [aiImgList, setAiImgList] = useState([
     {
@@ -199,22 +199,22 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
   ]);
   const [file, setFile] = useState(null);
   const [cantClick, setCantClick] = useState(false);
-  const [genLoading, setGenLoading] = useState(false);
+  const [genLoading, setGenLoading] = useState(true);
   const [genMintLoading, setGenMintLoading] = useState(false);
 
   const RequestAI = async () => {
     setCantClick(true)
     setGenLoading(true)
     console.log(formik.values.text)
-    if (formik.values.text===""){
+    if (formik.values.text === "") {
       await setModalText('Please describe your art');
       setOpen(true)
       setCantClick(false)
       setGenLoading(false)
       return
     }
-    if (userPoint<=0){
-      await setModalText('Oops, it appears that you run out of points.\n\n'+
+    if (userPoint <= 0) {
+      await setModalText('Oops, it appears that you run out of points.\n\n' +
         'Simply comment “I need more points + wallet address” under this tweet. And our moderators will send more to you soon :-)');
       setOpen(true)
       setCantClick(false)
@@ -247,14 +247,14 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
   const [userImage, setUserImage] = useState(false);
 
   const mintNFT = async () => {
-    if (undefined=== formik.values.checked){
+    if (undefined === formik.values.checked) {
       await setModalText('Please select one of the four images')
       setOpen(true)
       return
     }
     var imageString = formik.values.checked.pic;
 
-    if (formik.values.checked.seed<=4){
+    if (formik.values.checked.seed <= 4) {
       await setModalText('You don\'t have picture')
       setOpen(true)
       return
@@ -262,7 +262,7 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
     setCantClick(true)
     setGenMintLoading(true)
     try {
-      const url=await uploadToNFTStorage(DataURIToBlob(imageString))
+      const url = await uploadToNFTStorage(DataURIToBlob(imageString))
       // console.log(url)
       const data = {
         packageObjectId: '0xb161ddb6354c6610807dd2ca83a38be9621aee74',
@@ -272,7 +272,7 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
         arguments: [
           '0xd7a4fbd4c4c4fc16a5dbd88d045f0e4faacc31f0',
           'first nft',
-          "ipfs://"+url,
+          "ipfs://" + url,
         ],
         gasBudget: 10000,
       };
@@ -301,11 +301,11 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
   return (
     <>
       <div className={styles.firstContent}>
-        <BannerComp/>
+        <BannerComp />
         <div className={styles.right}>
-          <InfoComp/>
-          <TextInputComp formik={formik}/>
-          <UploadImageComp file={file} setFile={setFile}/>
+          <InfoComp />
+          <TextInputComp formik={formik} />
+          <UploadImageComp file={file} setFile={setFile} />
           <Button
             disabled={cantClick}
             onClick={RequestAI}
@@ -325,15 +325,18 @@ const AIGCMintContainer = ({formik, userPoint,setOpen,setModalText}) => {
             />}
           >
             <span>Generate ({userPoint} points remaining)</span>
-            {genLoading?<CircularProgress className={styles.generateLoading}/>:null}
+            {genLoading ? <CircularProgress className={styles.generateLoading} sx={{
+              width: '30px !important',
+              height: '30px !important',
+            }} /> : null}
           </Button>
-          <ImageSelectComp formik={formik} aiImgList={aiImgList}/>
+          <ImageSelectComp formik={formik} aiImgList={aiImgList} />
           <Button
             disabled={cantClick}
             className={styles.mintNowForFree}
             onClick={mintNFT}>
             Mint now for free
-            {genMintLoading?<CircularProgress className={styles.generateLoading}/>:null}
+            {genMintLoading ? <CircularProgress className={styles.generateLoading} /> : null}
           </Button>
           <AIGCSuccessModal setOpen={setSuccessOpen} userImage={userImage} text={""} open={successOpen} />
           {/*<AIGCModal setOpen={setOpen} text={modalText} open={open} />*/}
