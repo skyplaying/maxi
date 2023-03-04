f56bec9d55d128df02c18949d3e628e4393f2489
yechunsen <1604980041@qq.com>
2023-03-04 14:07:58 +0000

    add clip image

--- src/components/AIGC/CropComp.jsx
+++ src/components/AIGC/CropComp.jsx
@@ -2,8 +2,31 @@ import ReactCrop from 'react-image-crop'
 import React, { useEffect, useRef, useState } from 'react'
 import { Box, IconButton, Button } from '@mui/material'
 import deleteIcon from '../../assets/img/page/aigc/deleteIcon.svg'
+import confirmicon from '../../assets/img/page/aigc/confirmicon.svg'
 import styles from '../../pages/aigc/index.module.scss'
 
+const canvasToImage = function (image, crop) {
+  const canvas = document.createElement('canvas');
+  const scaleX = image.naturalWidth / image.width;
+  const scaleY = image.naturalHeight / image.height;
+  canvas.width = imageWidth;
+  canvas.height = imageWidth;
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(
+    image,
+    crop.x * scaleX,
+    crop.y * scaleY,
+    crop.width * scaleX,
+    crop.height * scaleY,
+    0,
+    0,
+    imageWidth,
+    imageWidth
+  );
+  const url = canvas.toDataURL('image/jpeg');
+  return url
+};
 const saveMyImage = function (canvas, filename) {
   let strData = canvas.toDataURL('image/jpeg');
   strData = strData.replace('image/jpeg', 'image/octet-stream');
@@ -84,6 +107,7 @@ function CropComp({ file, setFile }) {
     height: 512
   });
   const [src, setSrc] = useState('');
+  const [cropSrc, setCropSrc] = useState('');
   useEffect(() => {
     if (file) {
       const s = URL.createObjectURL(file);
@@ -92,60 +116,108 @@ function CropComp({ file, setFile }) {
   }, [file])
   return (
     <>
-      <Box
-        sx={{
-          width: 600,
-          minHeight: 600,
-          border: '1px solid #fff',
-          position: 'relative',
-          mt: 2,
-          // '.ReactCrop': {
-          //   height: '100%',
-          //   '.ReactCrop__child-wrapper': {
-          //     height: '100%',
-          //     img: {
-          //       height: '100%'
-          //     }
-          //   }
-          // }
-        }}
-      >
-        <ReactCrop
-          crop={crop}
-          onChange={c => {
-            setCrop({
-              ...c,
-              width: c.width,
-              height: c.width
-            })
-          }}
-        >
-          <Box
-            ref={imageRef}
-            src={src}
-            component='img'
-          />
-        </ReactCrop>
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-haspopup="true"
-          sx={{
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            width: 60,
-            height: 60,
-          }}
-          onClick={() => {
-            setFile(null);
-          }}
-        >
-          <img src={deleteIcon} />
-        </IconButton>
-      </Box>
+      {
+        cropSrc
+          ? <Box
+            sx={{
+              position: 'relative',
+              my: 2,
+            }}>
+            <Box
+              component='img'
+              src={cropSrc}
+            />
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-haspopup="true"
+              sx={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                width: 60,
+                height: 60,
+              }}
+              onClick={() => {
+                setCropSrc('');
+              }}
+            >
+              <img src={deleteIcon} />
+            </IconButton>
+          </Box>
+          : <Box
+            sx={{
+              width: 600,
+              minHeight: 600,
+              border: '1px solid #fff',
+              position: 'relative',
+              mt: 2,
+              // '.ReactCrop': {
+              //   height: '100%',
+              //   '.ReactCrop__child-wrapper': {
+              //     height: '100%',
+              //     img: {
+              //       height: '100%'
+              //     }
+              //   }
+              // }
+            }}
+          >
+            <ReactCrop
+              crop={crop}
+              onChange={c => {
+                setCrop({
+                  ...c,
+                  width: c.width,
+                  height: c.width
+                })
+              }}
+            >
+              <Box
+                ref={imageRef}
+                src={src}
+                component='img'
+              />
+            </ReactCrop>
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-haspopup="true"
+              sx={{
+                position: 'absolute',
+                right: 70,
+                top: 0,
+                width: 60,
+                height: 60,
+              }}
+              onClick={() => {
+                const url = canvasToImage(imageRef?.current, crop);
+                setCropSrc(url)
+              }}
+            >
+              <img src={confirmicon} />
+            </IconButton>
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-haspopup="true"
+              sx={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                width: 60,
+                height: 60,
+              }}
+              onClick={() => {
+                setFile(null);
+              }}
+            >
+              <img src={deleteIcon} />
+            </IconButton>
+          </Box>
+      }
 
-      <Button
+      {/* <Button
         variant="contained"
         className={styles.mintNowForFree}
         onClick={async () => {
@@ -171,7 +243,7 @@ function CropComp({ file, setFile }) {
         }}
       >
         Get Blob
-      </Button>
+      </Button> */}
     </>
   )
 }
